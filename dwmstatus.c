da0ffe4ae8139ef5d9d49cc9055c36cf3f2b8af7
Daman Mohan Kumar <kumardm@uci.edu>
2020-05-22 17:29:15 -0700

    Add customised suckless source code

---
+++ dwmstatus.c
@@ -0,0 +1,210 @@
+/*
+ * Copy me if you can.
+ * by 20h
+ */
+
+#define _BSD_SOURCE
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/time.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <X11/Xlib.h>
+
+char *tzist = "Asia/Kolkata";
+char *tzberlin = "Europe/Berlin";
+char *TZ = "TZ";
+
+static Display *dpy;
+
+char *
+smprintf(char *fmt, ...)
+{
+	va_list fmtargs;
+	char *ret;
+	int len;
+
+	va_start(fmtargs, fmt);
+	len = vsnprintf(NULL, 0, fmt, fmtargs);
+	va_end(fmtargs);
+
+	ret = malloc(++len);
+	if (ret == NULL) {
+		perror("malloc");
+		exit(1);
+	}
+
+	va_start(fmtargs, fmt);
+	vsnprintf(ret, len, fmt, fmtargs);
+	va_end(fmtargs);
+
+	return ret;
+}
+
+void
+settz(char *tzname)
+{
+	setenv(TZ, tzname, 1);
+}
+
+void
+unsettz(char *tzname)
+{
+	unsetenv(TZ);
+}
+
+char *
+mktimes(char *fmt, char *tzname)
+{
+	char buf[129];
+	time_t tim;
+	struct tm *timtm;
+
+	if (tzname) {
+		settz(tzname);
+	}
+	tim = time(NULL);
+	timtm = localtime(&tim);
+	if (tzname) {
+		unsettz(tzname);
+	}
+	if (timtm == NULL)
+		return smprintf("");
+
+	if (!strftime(buf, sizeof(buf)-1, fmt, timtm)) {
+		fprintf(stderr, "strftime == 0\n");
+		return smprintf("");
+	}
+
+	return smprintf("%s", buf);
+}
+
+void
+setstatus(char *str)
+{
+	XStoreName(dpy, DefaultRootWindow(dpy), str);
+	XSync(dpy, False);
+}
+
+char *
+loadavg(void)
+{
+	double avgs[3];
+
+	if (getloadavg(avgs, 3) < 0)
+		return smprintf("");
+
+	return smprintf("%.2f %.2f %.2f", avgs[0], avgs[1], avgs[2]);
+}
+
+char *
+readfile(char *base, char *file)
+{
+	char *path, line[513];
+	FILE *fd;
+
+	memset(line, 0, sizeof(line));
+
+	path = smprintf("%s/%s", base, file);
+	fd = fopen(path, "r");
+	free(path);
+	if (fd == NULL)
+		return NULL;
+
+	if (fgets(line, sizeof(line)-1, fd) == NULL)
+		return NULL;
+	fclose(fd);
+
+	return smprintf("%s", line);
+}
+
+char *
+getbattery(char *base)
+{
+	char *co, status;
+	int cap;
+
+	cap = -1;
+
+	co = readfile(base, "present");
+	if (co == NULL)
+		return smprintf("");
+	if (co[0] != '1') {
+		free(co);
+		return smprintf("not present");
+	}
+	free(co);
+
+	co = readfile(base, "capacity");
+	if (co == NULL) {
+		return smprintf("");
+	}
+	sscanf(co, "%d", &cap);
+	free(co);
+
+	co = readfile(base, "status");
+	if (!strncmp(co, "Discharging", 11)) {
+		status = '-';
+	} else if(!strncmp(co, "Charging", 8)) {
+		status = '+';
+	} else {
+		status = '?';
+	}
+	free(co);
+
+	if (cap < 0)
+		return smprintf("invalid");
+
+	return smprintf("%d%%%c", cap, status);
+}
+
+char *
+gettemperature(char *base, char *sensor)
+{
+	char *co;
+
+	co = readfile(base, sensor);
+	if (co == NULL)
+		return smprintf("");
+	return smprintf("%02.0f°C", atof(co) / 1000);
+}
+
+int
+main(void)
+{
+	char *status;
+	char *bat;
+	char *tmlocal;
+	char *tmist;
+
+	if (!(dpy = XOpenDisplay(NULL))) {
+		fprintf(stderr, "dwmstatus: cannot open display.\n");
+		return 1;
+	}
+
+	for (;;sleep(2)) {
+		bat = getbattery("/sys/class/power_supply/BAT0");
+		tmlocal = mktimes("%a %d %b %H:%M %Z", NULL);
+		tmist = mktimes("%H:%M", tzist);
+
+		status = smprintf("B: %s | IST: %s | Local: %s",
+				  bat, tmist, tmlocal);
+		setstatus(status);
+
+		free(bat);
+		free(tmlocal);
+		free(tmist);
+		free(status);
+	}
+
+	XCloseDisplay(dpy);
+
+	return 0;
+}
+
